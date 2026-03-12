a2aee60d6276bb3bcdd73b58bb0f41f9c0585a77
Ernest Tan Chang Ler <ernest36912@gmail.com>
2026-03-13 01:03:07 +0800

    fix: made alfred's changes the initial schema and inital migration.

--- Data/UnitOfWork/AppDbContext.cs
+++ Data/UnitOfWork/AppDbContext.cs
@@ -173,7 +173,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pro_rental;Username=postgres;Password=132135");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pro_rental;Username=postgres;Password=password");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
